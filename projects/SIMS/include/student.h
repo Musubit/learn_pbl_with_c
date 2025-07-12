4317b3c8e827237f96675dd9db2c6c750766efc9
Musubi <15171349353@163.com>
2025-07-12 10:33:45 +0800

    建立学生信息结构体，声明函数

---
+++ projects/SIMS/include/student.h
@@ -0,0 +1,22 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+typedef struct Student {
+    int id;
+    char name[100];
+    int age;
+    float score;
+    struct Student* next;
+} Student;
+
+extern Student* head;
+
+Student* createStudent(int id, const char* name, int age, float score);
+void addStudent(Student* student);
+void deleteStudent(int id);
+void updateStudent(int id, const char* name, int age, float score);
+Student* findStudent(int id);
+void displayAllStudents();
+void freeList();
+
+#endif // STUDENT_H
